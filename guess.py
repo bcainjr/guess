92549ed7880eccd06b99d96b9fdd257ac0838f2b
Bruce Cain <brucecainjr@gmail.com>
2018-06-21 14:55:12 -0400

    Created guess.py project

---
+++ guess.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#
+# Python
+# Project 2
+#
+# UMBC TDQC5
+# Bruce Cain
+#
+# Create a Guessing game.
+# The game will pick a number between 1 and 100.
+# Prompt the user for a number and if its the correct number the user won.
+# If its higher or lower the the computer will indicate as so.
+
+
+import random
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
